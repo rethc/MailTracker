930c29028790e102ab5bb82a30c8965d020fa71d
rethc <reth@xtra.co.nz>
2022-06-14 09:13:16 +1200

    install muix-date-pickers. Added filter for date

--- MailTracker/ClientApp/src/components/Search.jsx
+++ MailTracker/ClientApp/src/components/Search.jsx
@@ -3,6 +3,7 @@ import {
   Container,
   CssBaseline, 
   Box,
+  TextField,
 } from "@mui/material";
 import React, { useState, useEffect } from "react"; 
 import { format, zonedTimeToUtc } from "date-fns-tz";
@@ -10,10 +11,17 @@ import { parseISO } from "date-fns";
 import Copyright from "./Copyright";
 import MUIDataTable from "mui-datatables";
 import axios from "axios";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 export default function Search() {
   const [mailList, setMailList] = useState([]);
   const [isLoading, setLoading] = useState(true); //loading spinner
+  const [dateValue, setDateValue] = useState(null);
+  
+  const handleDateChange = (newValue) => {
+      setDateValue(newValue);
+    };
 
   async function fetchData() {
     const { data } = await axios.get(
@@ -56,10 +64,66 @@ export default function Search() {
       name: "dateCreated",
       label: "Date Scanned",
       options: {
-        filter: false,
+        filter: true,
         sort: true,
-      },
-    },
+        display: "true",
+        filterType: "custom", customFilterListOptions: {
+          render: (value) => {
+            if (isNaN(value[0]) || !value[0]) return [];
+            return [format(value[0], "dd/MM/yyyy")];
+          },
+          update: (filterList, filterPos, index) => {
+            console.log(
+              "customFilterListOnDelete: ",
+              filterList,
+              filterPos,
+              index
+            );
+
+            if (filterPos === 0) {
+              filterList[index].splice(filterPos, 1, "");
+            } else if (filterPos === 1) {
+              filterList[index].splice(filterPos, 1);
+            } else if (filterPos === -1) {
+              filterList[index] = [];
+            }
+
+            return filterList;
+          }
+        },
+        filterOptions: {
+          logic: (dateTime, filters) => {
+            if (isNaN(filters[0])) return false;
+            let date = filters[0] && format(filters[0], "dd/MM/yyyy");
+            let dateString = dateTime.split(" ")[0];
+            console.log(dateString);
+            if (dateString === date) {
+              return false;
+            } else if (!date) {
+              return false;
+            }
+            return true;
+          },
+          display: (filterList, onChange, index, column) => {
+            return (
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Filter by Date"
+                  inputFormat="dd/MM/yyyy"
+                  value={filterList[index][0] || null}
+                  onChange={(e) => {
+                    handleDateChange(e);
+                    filterList[index][0] = e;
+                    onChange(filterList[index], index, column);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            );
+          }
+        }
+      }
+    }
   ];
 
   const data = mailList
@@ -78,7 +142,7 @@ export default function Search() {
     .reverse();
 
   const options = {
-    filterType: "checkbox",
+    filterType: "dropdown",
     customToolbarSelect: () => {},
     print: false,
     searchOpen: true,
