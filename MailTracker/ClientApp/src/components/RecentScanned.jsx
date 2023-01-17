2226b7b3b688ed77ba85f352da34323b27b5ce7e
rethc <reth@xtra.co.nz>
2023-01-18 00:09:42 +1300

    Update packages

--- MailTracker/ClientApp/src/components/RecentScanned.jsx
+++ MailTracker/ClientApp/src/components/RecentScanned.jsx
@@ -5,6 +5,7 @@ import {
     TableContainer,
     TableHead,
     TableRow,
+    Typography,
   } from "@mui/material";
   import React from "react";
 import { format, zonedTimeToUtc } from "date-fns-tz";
@@ -28,35 +29,35 @@ import { parseISO } from "date-fns";
                 )}
                 <TableCell>Date Scanned</TableCell>
               </TableRow>
-            </TableHead> 
-              <TableBody>
-                {mailList &&
-                  mailList.map((m) => (
-                    <TableRow key={m.externalMailID}>
-                      {/* If tracking number is longer than 30 characters, truncate and append ...*/}
-                      <TableCell>
-                        {m.trackingNo.length > 80
-                          ? `${m.trackingNo.substring(0, 80)}...`
-                          : m.trackingNo}
-                      </TableCell>
-                      <TableCell>{m.mailType}</TableCell>
-                      {props.type === "Incoming" ? (
-                        <TableCell>{m.productType}</TableCell>
-                      ) : (
-                        <React.Fragment></React.Fragment>
+            </TableHead>
+            <TableBody>
+              {mailList &&
+                mailList.map((m) => (
+                  <TableRow key={m.externalMailID}>
+                    {/* If tracking number is longer than 30 characters, truncate and append ...*/}
+                    <TableCell>
+                      {m.trackingNo.length > 80
+                        ? `${m.trackingNo.substring(0, 80)}...`
+                        : m.trackingNo}
+                    </TableCell>
+                    <TableCell>{m.mailType}</TableCell>
+                    {props.type === "Incoming" ? (
+                      <TableCell>{m.productType}</TableCell>
+                    ) : (
+                      <React.Fragment></React.Fragment>
+                    )}
+                    <TableCell>
+                      {/* Returns a Date with the UTC time. date-fns-tz library will display the date and time in the local time of the user */}
+                      {format(
+                        zonedTimeToUtc(parseISO(m.dateCreated), "UTC"),
+                        "dd/MM/yyyy hh:mm aaa"
                       )}
-                      <TableCell>
-                        {/* Returns a Date with the UTC time. date-fns-tz library will display the date and time in the local time of the user */}
-                        {format(
-                          zonedTimeToUtc(parseISO(m.dateCreated), "UTC"),
-                          "dd/MM/yyyy hh:mm aaa"
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody> 
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
           </Table>
-        </TableContainer>
+        </TableContainer> 
       </React.Fragment>
     );
   }
